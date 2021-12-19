900eeb3ce1a1a8b9dc53e90ff21dc87ca6f5e839
ayousry42 <ayousry42@gmail.com>
2021-12-19 21:39:43 +0200

    Update yousri

--- Data.R
+++ Data.R
@@ -1,14 +1,23 @@
-library(readxl)
-#grocery_entries <- read.csv(file.choose())
-grocery_entries <- read_excel("~/College/Data Science/Project/grc.xlsx")
+# Installing Packages
+install.packages("ClusterR")
+install.packages("cluster")
+
+# Loading package
+library(ClusterR)
+library(cluster)
+
+grocery_entries <- read.csv(file.choose())
+#grocery_entries <- read_excel("~/College/Data Science/Project/grc.xlsx")
 cash_credit <- cbind(grocery_entries[3], grocery_entries[8])
 #
 #sum_cash <-sum(cash_credit[which(cash_credit$paymentType=='Cash'),1])
 #sum_credit <-sum(cash_credit[which(cash_credit$paymentType=='Credit'),1])
 
-CompCashCredit <- c(sum_cash,sum_credit)
+#CompCashCredit <- c(sum_cash,sum_credit)
 #please visualise the data I am lazy - Sewelam
 ages <- cbind(grocery_entries[6] , grocery_entries[3])
 sum_ages <- aggregate(total ~ age,ages,sum)
+plot(sum_ages)
 #First Github Commitment! 23
 #Don't be lazy swelam --Yousri
+table()
